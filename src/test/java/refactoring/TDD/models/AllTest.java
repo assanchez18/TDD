1190a136f40f2663fda5941f92336f2e2433a282
Sergio Sanchez <assanchez18@gmail.com>
2019-05-30 19:49:32 +0200

    Includes AllTest class

---
+++ src/test/java/refactoring/TDD/models/AllTest.java
@@ -0,0 +1,16 @@
+package refactoring.TDD.models;
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ 
+	OpenedIntervalTest.class,
+	ClosedIntervalTest.class
+})
+
+public class AllTest {
+
+}
